8b4b3ca77c3665b6fa7717964a207b4ced1fee6d
Ubuntu <ubuntu@ip-10-0-210-8.ap-south-1.compute.internal>
2022-02-20 23:41:39 +0000

    Test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My exxpress' });
+  res.render('index', { title: 'My exsdjhsdjxpress' });
 });
 
 module.exports = router;
